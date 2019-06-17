29c4a8b381805b1dd0f0f795b94cc422f8f2feab
shoyeb <memonshoyeb26@gmail.com>
2019-06-17 15:19:42 +0530

    complete simple jwt

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ app.get('/api', (req,res) => {
 });
 
 app.post('/api/posts',verifyAuth, (req,res) => {
-    res.json({
-        message: 'Post Created'
+    jwt.verify(req.token, 'sk' , (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        }else {
+            res.json({
+                message: 'Post Created',
+                authData: authData
+            });
+        }
     });
 });
 
@@ -24,7 +31,7 @@ app.post('/api/login', (req,res) => {
         email : 'memonshoyeb26@gmail.com'
     }
 
-    jwt.sign({user : USER} ,'seckretkey' , (err, token)=> {
+    jwt.sign({user : USER} ,'sk' , (err, token)=> {
         res.json({
             token : token
         })
